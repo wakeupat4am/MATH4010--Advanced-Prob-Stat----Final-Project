57c27dc2f82112b75ba92786a066af3fc7a3868d
Pham Dinh Hieu <dinhieufam@Mac.lan>
2026-05-27 21:56:22 +0700

    feat: update new data and add the record length sensitivity analysis

--- Data/q3/download_data.sh
+++ Data/q3/download_data.sh
@@ -4,5 +4,5 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
 
-gdown --folder https://drive.google.com/drive/folders/18S-Q2UA7w5cjqkmbkbxgyxvhvpCbEQbg?usp=drive_link
-gdown --folder https://drive.google.com/drive/folders/14xQKGheTF8nNUjMTYfeMopH8998bD7SI?usp=drive_link
+gdown --folder https://drive.google.com/drive/folders/1g1WC5gS1_KBApARnSsW1yaPUIEvtMDgC?usp=drive_link
+gdown --folder https://drive.google.com/drive/folders/1ZWieSC5SMsLKtMkBedXR0_QPu1Rxj8QY?usp=drive_link
